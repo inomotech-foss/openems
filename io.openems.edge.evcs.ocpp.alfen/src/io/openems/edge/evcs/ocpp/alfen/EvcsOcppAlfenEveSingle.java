88a8fb99683931105b76a0409ec566ba4f85fa86
MurselKhan <mursel.khan@b-on.com>
2025-05-26 17:46:50 +0200

    Import dependency missing for OCPP server before merge!

--- io.openems.edge.evcs.ocpp.alfen/src/io/openems/edge/evcs/ocpp/alfen/EvcsOcppAlfenEveSingle.java
+++ io.openems.edge.evcs.ocpp.alfen/src/io/openems/edge/evcs/ocpp/alfen/EvcsOcppAlfenEveSingle.java
@@ -1,26 +1,22 @@
 package io.openems.edge.evcs.ocpp.alfen;
 
 import org.osgi.service.event.EventHandler;
-
 import io.openems.edge.common.channel.Doc;
 import io.openems.edge.common.component.OpenemsComponent;
 import io.openems.edge.evcs.api.Evcs;
 import io.openems.edge.evcs.api.ManagedEvcs;
 import io.openems.edge.evcs.api.MeasuringEvcs;
+import io.openems.edge.meter.api.ElectricityMeter;
 
-public interface EvcsOcppAlfenEveSingle extends Evcs, MeasuringEvcs, ManagedEvcs, OpenemsComponent, EventHandler {
+/** Marker-Interface für die Alfen-Komponente. */
+public interface EvcsOcppAlfenEveSingle extends Evcs, MeasuringEvcs, ManagedEvcs,
+                                                ElectricityMeter, OpenemsComponent,
+                                                EventHandler {
 
-	public enum ChannelId implements io.openems.edge.common.channel.ChannelId {
-		;
-		private final Doc doc;
+    /** Keine Alfen-spezifischen Kanäle nötig. */
+    enum ChannelId implements io.openems.edge.common.channel.ChannelId {
+        ;
 
-		private ChannelId(Doc doc) {
-			this.doc = doc;
-		}
-
-		@Override
-		public Doc doc() {
-			return this.doc;
-		}
-	}
+        @Override public Doc doc(){ return null; }
+    }
 }
