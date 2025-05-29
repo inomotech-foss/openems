cf4082cec5372ea46fe247e464d730bea7271f5f
Mursel Khan <mursel.khan@b-on.com>
2025-05-29 20:31:02 +0200

    fix error

--- io.openems.edge.evcs.keba.kecontact/src/io/openems/edge/evcs/keba/p40/EvcsKebaP40Impl.java
+++ io.openems.edge.evcs.keba.kecontact/src/io/openems/edge/evcs/keba/p40/EvcsKebaP40Impl.java
@@ -122,7 +122,7 @@ public class EvcsKebaP40Impl extends AbstractOpenemsModbusComponent implements E
 				config.modbus_id());
 
 		this.config = config;
-		this._setPowerPrecision(0.23);
+		this._setPowerPrecision(Evcs.DEFAULT_POWER_PRECISION);
 		this._setChargingType(ChargingType.AC);
 		this._setFixedMinimumHardwarePower(this.getConfiguredMinimumHardwarePower());
 		this._setFixedMaximumHardwarePower(this.getConfiguredMaximumHardwarePower());
